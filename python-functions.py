cdbf6428271cc374695aadd495963c3a3a0c8040
vinnietito <vincentnjuguna69@gmail.com>
2024-11-13 11:48:22 +0300

    Commits

--- python-functions.py
+++ python-functions.py
@@ -5,8 +5,14 @@
 
 #Passing of arguments and parameters in Python
 #lName = "Kimanthi"
-def my_func(fName, lName, age):
+#def my_func(fName, lName, age):
     #print(fName + " it's awesome")
-    print(f"{fName} {lName} is {age} years old.")
+#     print(f"{fName} {lName} is {age} years old.")
 
-my_func("Vincent", "Kimanthi", 20)
+# my_func("Vincent", "Kimanthi", 20)
+
+#Function that collect user inputs
+def myUser_func(fName):
+    return "Hello " + fName
+greet = myUser_func(input("Enter your name: "))
+print(greet)
