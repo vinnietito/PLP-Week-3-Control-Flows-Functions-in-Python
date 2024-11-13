dd87d36eb3d636a91f0d3eec28082bb8a95b660e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-13 11:08:17 +0300

    Commits

--- for-loop.py
+++ for-loop.py
@@ -1,2 +1,10 @@
-for i in range(10):
+# for i in range(10):
+#     print(i+1)
+
+# Print the first 10 multiples of 2
+# for i in range(1, 5):
+#     print(i * 2)
+
+
+for i in range(1, 11, 2):
     print(i+1)
